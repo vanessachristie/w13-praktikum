a4e9f1882051db2eb0361ae6fc83d23931df8ad6
vanessachristie <vchristie@student.ciputa.ac.id>
2021-12-06 11:39:43 +0700

    PRAKTIKUM

--- w13 praktikum/Program.cs
+++ w13 praktikum/Program.cs
@@ -8,22 +8,22 @@ namespace w13_praktikum
         static void Main(string[] args)
         {
             int statusProgram = 1;
+            Console.WriteLine("Berapa data");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            string[] koleksiNIM = new string[1000];
+            string[,] list = new string[1000, 6];
+            var arrayTerakhir = 1;
             while (statusProgram == 1)
             {
-                Console.WriteLine("Berapa data");
-                int input2 = Convert.ToInt32(Console.ReadLine());
-                string[] koleksiNIM = new string[1000];
-                string[,] list = new string[1000, 6];
-                var arrayTerakhir = 1;
                 for (int i = arrayTerakhir; i <= input2; i++)
                 {
                     Console.WriteLine();
                     Console.WriteLine($"Data ke- {i} ");
                     Console.Write("NIM           :");
                     string nim = Convert.ToString(Console.ReadLine());
-                    for (int c = 0; c < koleksiNIM.Length; c++)
+                    for (int c = i; c < koleksiNIM.Length; c++)
                     {
-                        while (nim == koleksiNIM[c])
+                        if (nim == koleksiNIM[c])
                         {
                             Console.WriteLine("NIM sudah ada !!!!");
                             Console.Write("NIM           :");
@@ -57,24 +57,26 @@ namespace w13_praktikum
                     string year = "TAHUN MASUK";
                     string prodi1 = "PROGRAM STUDI";
                     string kelas1 = "KELAS";
-                    Console.WriteLine(String.Format("{0,-7}{1,-3}{2,-20}{3,10}  {4,-10}  {5,-15}{6,-5}", nomor, number, name, jenis1, year, prodi1, kelas1));
-                    for (int i = 0; i < input2; i++)
+                    Console.WriteLine(String.Format("{0,-7}{1,-5}{2,-20}{3,-20}  {4,-10}  {5,-20}{6,-10}", nomor, number, name, jenis1, year, prodi1, kelas1));
+                    for (int i = 1; i <= input2; i++)
                     {
-                        Console.WriteLine(String.Format("{0,-7}{1,-3}{2,-20}{3,10}  {4,-10}  {5,-15}{6,-5}", i + 1, koleksiNIM[0], list[i, 0], list[i, 1], list[i, 2], list[i, 3], list[i, 4]));
+                        Console.WriteLine(String.Format("{0,-7}{1,-5}{2,-20}{3,-20}  {4,-10}  {5,-20}{6,-10}", i , koleksiNIM[i], list[i, 0], list[i, 1], list[i, 2], list[i, 3], list[i, 4]));
                     }
                 }
                 if (pilihan == "n")
                 {
-                    statusProgram = 2;
+                    break;
                 }
                 Console.WriteLine();
                 Console.Write("Tambah Data ? (y/n) ...");
                 string pilihan2 = Convert.ToString(Console.ReadLine());
+               
                 if (pilihan2 == "y")
                 {
                     Console.Write("Berapa data= ");
                     int lala = Convert.ToInt32(Console.ReadLine());
-                    arrayTerakhir = input2;
+                    list = new string[input2 + lala, 6];
+                    arrayTerakhir = input2+1;
                     input2 = input2 + lala;
                 }
 
