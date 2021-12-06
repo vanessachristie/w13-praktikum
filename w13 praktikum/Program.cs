84b1e8cc5887a6776e18138d69ad604a3c9438b9
vanessachristie <vchristie@student.ciputa.ac.id>
2021-12-06 07:22:17 +0700

    Add project files.

---
+++ w13 praktikum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace w13_praktikum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
